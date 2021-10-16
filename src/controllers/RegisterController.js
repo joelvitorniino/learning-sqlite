7f3e62c3e848df7d12b8da60c9d847ca3829d34b
joelvitorniino <joelprofessor13@gmail.com>
2021-10-16 19:31:37 -0300

    adding route findById

--- src/controllers/RegisterController.js
+++ src/controllers/RegisterController.js
@@ -1,51 +1,54 @@
 class RegisterController {
-    index(db, req, res) {
-        db.all(`SELECT * FROM register`, (err, rows) => {
-            try {
-                res.json(rows);
-            } catch {
-                console.error(err);
-            };
-        });
-    };
+  index(db, req, res) {
+    db.all(`SELECT * FROM register`, (err, rows) => {
+      try {
+        res.json(rows);
+      } catch {
+        console.error(err);
+      }
+    });
+  };
 
-    store(db, req, res) {
-        const {
-            name,
-            birth,
-            nationality,
-            gender,
-            email
-        } = req.body;
+  findById(db, req, res) {
+    const { id } = req.params;
+    db.each(`SELECT * FROM register WHERE id = ${id}`, (err, rows) => {
+      try {
+        res.json(rows);
+      } catch {
+        console.error(err);
+      }
+    });
+  };
 
-        const stmt = db.prepare(`INSERT INTO register (name, birth, nationality, gender, email) VALUES(?, ?, ?, ?, ?)`);
-        stmt.run(name, birth, nationality, gender, email);
-        stmt.finalize();
-        res.json({ data: "Register added on success"});
-    };
+  store(db, req, res) {
+    const { name, birth, nationality, gender, email } = req.body;
 
-    update(db, req, res) {
-        const {
-            name,
-            birth,
-            nationality,
-            gender,
-            email
-        } = req.body;
-        const { id } = req.params;
-        const stmt = db.prepare(`UPDATE register SET name = ?, birth = ?, nationality = ?, gender = ?, email = ? WHERE id = ?`);
-        stmt.run(name, birth, nationality, gender, email, id);
-        stmt.finalize();
-        res.json({ data: "Register updated on success"});
-    };
+    const stmt = db.prepare(
+      `INSERT INTO register (name, birth, nationality, gender, email) VALUES(?, ?, ?, ?, ?)`
+    );
+    stmt.run(name, birth, nationality, gender, email);
+    stmt.finalize();
+    res.json({ data: "Register added on success" });
+  };
 
-    delete(db, req, res) {
-        const { id } = req.params;
-        const stmt = db.prepare(`DELETE FROM register WHERE id = ?`);
-        stmt.run(id);
-        stmt.finalize();
-        res.json({ data: "Register deleted on success"});
-    };
+  update(db, req, res) {
+    const { name, birth, nationality, gender, email } = req.body;
+    const { id } = req.params;
+    const stmt = db.prepare(
+      `UPDATE register SET name = ?, birth = ?, nationality = ?, gender = ?, email = ? WHERE id = ?`
+    );
+    stmt.run(name, birth, nationality, gender, email, id);
+    stmt.finalize();
+    res.json({ data: "Register updated on success" });
+  };
+
+  delete(db, req, res) {
+    const { id } = req.params;
+    const stmt = db.prepare(`DELETE FROM register WHERE id = ?`);
+    stmt.run(id);
+    stmt.finalize();
+    res.json({ data: "Register deleted on success" });
+  };
 };
 
 module.exports = RegisterController;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,6 +5,7 @@ const RegisterController = require('../controllers/RegisterController');
 const Register = new RegisterController();
 
 routes.get('/register', (req, res) => Register.index(db, req, res));
+routes.get('/register/:id', (req, res) => Register.findById(db, req, res));
 routes.post('/register', (req, res) => Register.store(db, req, res));
 routes.put('/register/:id', (req, res) => Register.update(db, req, res));
 routes.delete('/register/:id', (req, res) => Register.delete(db, req, res));
